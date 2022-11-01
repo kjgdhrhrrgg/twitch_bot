5c97ff134e7f55c720e78a836bd02e7806097f8c
kjgdhrhrrgg <d.mai-tran@gmx.de>
2022-11-01 20:41:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -406,7 +406,7 @@ function database(context, argument) {
 	
 	if (argument == null || argument == "") argument = context.username.toLowerCase();
 	if (argument.charAt(0)== '@') argument = argument.substring(1);
-	var db_msg = `${argument} is not in the database. Message @kjg_mk8dx on twitter, so he can add you to the database`;
+	var db_msg = `${argument} is not in the database. Message @kj_mk8dx on twitter, so he can add you to the database`;
 	if (playerList.some(row => row.includes(argument.toLowerCase()))) db_msg = `${argument} is in the database.`
 	return db_msg;
 
