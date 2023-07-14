1eecb65458371bc9c41d2282b7d8056588b4271c
kjgdhrhrrgg <d.mai-tran@gmx.de>
2023-07-14 09:35:45 +0200

    Update getCalc.js

--- functions/getCalc.js
+++ functions/getCalc.js
@@ -5,6 +5,7 @@ module.exports = function(mk8_api_table, numbercheck){
 		let calc_message;
 		let id;
 		if (argument == null || argument == "") calc_message = "Not enough data was given";
+		// if argument is talbeiD
 		else if (numbercheck(argument)) {
 			id = mk8_api_table+argument;
 			let xhr = new XMLHttpRequest();
@@ -38,19 +39,27 @@ module.exports = function(mk8_api_table, numbercheck){
 			xhr.send();
 	
 		}
+		// if argument contains data to calculate
 		else if (argument.includes(",")) {
 			let arg = [];
 			let teamsize= [1,2,3,4,6];
 
+			//constants for calculation
+			let caps = {1: 60, 2: 120, 3: 180, 4: 240, 6: 300};
+			let offsets = {1: 9500, 2: 5500, 3: 5100, 4: 4800, 6: 4650};
+			let scalingFactors = {1: 2746.116, 2: 1589.856, 3: 1474.230, 4: 1387.511, 6: 1344.151};
+
 			arg = argument.split(",")
-			if (arg.length != 14) calc_message = "Please provide the right amount of data. (!lm <teamsize>,<teamplacement>, 12 player names split with by an comma)"
+			if (arg.length != 14) calc_message = "Please provide the right amount of data. (!lm <teamsize>,<your_teamplacement>, 12 player_names split by a comma)"
 			if (numbercheck(arg[0]) && numbercheck(arg[1])) {
-				arg[0] = parseInt(arg[0]);
-				arg[1] = parseInt(arg[1]);
-				names = []
+				let current_teamsize = parseInt(arg[0]);
+				let your_teamplacement = parseInt(arg[1]);
 				if (teamsize.includes(arg[0]) && arg[1]<=12/arg[0] && arg[1]>0) {
-					//code to calculate starts here
+					let mmr_list = []
+					let teammate_counter = 0
+					//grab current mmr for each player and push to names, might calculate the avg team mmr + room mmr
 					for (let i = 2; i <= 14; i++) {
+						
 						let xhr = new XMLHttpRequest();
 						id = mk8_api_table+arg[i];
 						xhr.open("GET", id, false);
@@ -63,11 +72,28 @@ module.exports = function(mk8_api_table, numbercheck){
 							if (xhr.readyState == 4 && xhr.status == 200) {
 								let json_data = JSON.parse(this.responseText);
 								calc_message = `Expected MMR Changes for TableID ${argument}: `		
-								names.push(json_data.mmr)
+								mmr_list.push(json_data.mmr)
 							}
 						}
 						xhr.send();
 					}
+					// calculate room mmr + team_mmr
+					let room_mmr = 0;
+					let team_mmr = [];
+					let current_team_mmr = 0;
+					for (i in mmr_list) {
+						room_mmr += mmr_list[i];
+						current_team += mmr_list[i];
+						teammate_counter++;
+						if (teammate_counter == current_teamsize) {
+							team_mmr.push(current_team_mmr/current_teamsize);
+							current_team_mmr = 0;
+							teammate_counter = 0;
+						}
+						if (i == 11) room_mmr = room_mmr/(12/current_teamsize);
+					}
+					let your_teamm_mmr = team_mmr[your_teamplacement-1];
+					console.log(your_teamm_mmr, team_mmr, room_mmr);
 				}
 				else console.log("pass unsuccessful");//
 			}
@@ -77,5 +103,4 @@ module.exports = function(mk8_api_table, numbercheck){
 		}
 		return calc_message;
 	}
-	
 };
